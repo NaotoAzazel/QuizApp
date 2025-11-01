1ec56f46a2c60a0a9e7e21ae9d2e20bea63f07ab
Vladislav <danirucakov@gmail.com>
2025-11-01 22:36:16 +0200

    added category selector component

--- QuizApp/ViewModels/StartQuizWindow.xaml.cs
+++ QuizApp/ViewModels/StartQuizWindow.xaml.cs
@@ -1,7 +1,5 @@
 ﻿using QuizApp.Common.Constants;
 using QuizApp.Models;
-using QuizApp.Services;
-using QuizApp.Services.Repositories;
 using System.Windows;
 
 namespace QuizApp.Views.Windows
@@ -13,38 +11,11 @@ namespace QuizApp.Views.Windows
         public StartQuizWindow()
         {
             InitializeComponent();
-
-            Loaded += StartQuizWindow_Loaded;
-        }
-
-        private void StartQuizWindow_Loaded(object sender, RoutedEventArgs e)
-        {
-            QuizCategoryRepository quizCategoryRepository = new QuizCategoryRepository(new DatabaseContext());
-            List<Category> categories = quizCategoryRepository.GetAll().ToList();
-            categories.Insert(0, new Category
-            {
-                Id = QuizSettings.MIXED_CATEGORY_ID,
-                Name = QuizSettings.MIXED_CATEGORY_NAME
-            });
-
-            if (categories.Any())
-            {
-                CategoryComboBox.ItemsSource = categories;
-                CategoryComboBox.SelectedIndex = 0;
-                CategoryComboBox.IsEnabled = true;
-                EmptyMessageTextBlock.Visibility = Visibility.Collapsed;
-            }
-            else
-            {
-                CategoryComboBox.ItemsSource = null;
-                CategoryComboBox.IsEnabled = false;
-                EmptyMessageTextBlock.Visibility = Visibility.Visible;
-            }
         }
 
         private void StartQuiz_Click(object sender, RoutedEventArgs e)
         {
-            SelectedCategory = CategoryComboBox.SelectedItem as Category;
+            SelectedCategory = CategorySelectorControl.GetSelectedCategory();
 
             if (SelectedCategory == null)
             {
---
+++ QuizApp/Views/Components/CategorySelector.xaml.cs
@@ -0,0 +1,59 @@
+﻿using QuizApp.Common.Constants;
+using QuizApp.Models;
+using QuizApp.Services;
+using QuizApp.Services.Repositories;
+using System.Windows.Controls;
+
+namespace QuizApp.Views.Components
+{
+    public partial class CategorySelector : UserControl
+    {
+        public event Action<Category?>? CategorySelected;
+
+        public CategorySelector()
+        {
+            InitializeComponent();
+            Loaded += (_, __) => LoadCategories();
+        }
+
+        private void LoadCategories()
+        {
+            QuizCategoryRepository quizCategoryRepository = new QuizCategoryRepository(new DatabaseContext());
+            List<Category> categories = quizCategoryRepository.GetAll().ToList();
+
+            if (categories.Any())
+            {
+                categories.Insert(0, new Category
+                {
+                    Id = QuizSettings.MIXED_CATEGORY_ID,
+                    Name = QuizSettings.MIXED_CATEGORY_NAME
+                });
+
+                CategoryComboBox.ItemsSource = categories;
+                CategoryComboBox.SelectedIndex = 0;
+                CategoryComboBox.IsEnabled = true;
+            }
+            else
+            {
+                CategoryComboBox.ItemsSource = null;
+                CategoryComboBox.IsEnabled = false;
+            }
+        }
+
+        private void CategoryComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedCategory = CategoryComboBox.SelectedItem as Category;
+            CategorySelected?.Invoke(selectedCategory);
+        }
+
+        public Category? GetSelectedCategory()
+        {
+            return CategoryComboBox.SelectedItem as Category;
+        }
+
+        public void SetSelectedCategory(Category? category)
+        {
+            CategoryComboBox.SelectedItem = category;
+        }
+    }
+}
